5f79320390d71e8c0094271b9700c779d67245b9
Nathan Cai <wenjuncai97@gmail.com>
2022-02-15 23:57:27 -0500

    finished array methods and callbacks

--- index.js
+++ index.js
@@ -4,15 +4,16 @@ const { fifaData } = require('./fifa.js')
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 1: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Practice accessing data by console.log-ing the following pieces of data note, you may want to filter the data first 😉*/
-
+const fifa2014 = fifaData.filter(item => item.Year === 2014 && item.Stage === 'Final');
+// console.log(fifa2014);
 //(a) Home Team name for 2014 world cup final
-
+console.log(fifa2014[0]['Home Team Name']);
 //(b) Away Team name for 2014 world cup final
-
+console.log(fifa2014[0]['Away Team Name']);
 //(c) Home Team goals for 2014 world cup final
-
+console.log(fifa2014[0]['Home Team Goals']);
 //(d) Away Team goals for 2014 world cup final
-
+console.log(fifa2014[0]['Away Team Goals']);
 //(e) Winner of 2014 world cup final */
 
 
@@ -25,12 +26,9 @@ hint - you should be looking at the stage key inside of the objects
 */
 
 function getFinals(array) {
-   const newArray = array.filter(function(item) {
-       return item.Stage === 'Final';
-   })
+   const newArray = array.filter(data => data.Stage === 'Final') //the 'data' is just a placement word. could be item, banana, etc. same goes for array. those are just placement words that will be invoked as arguments.
    return newArray;
 }
-
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Use the higher-order function called getYears to do the following: 
 1. Receive an array
@@ -41,8 +39,6 @@ function getYears(array, getFinalsCB) {
     return getFinalsCB(array).map(item => item.Year);
 }
 console.log(getYears(fifaData, getFinals));
-
-
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 4: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Use the higher-order function getWinners to do the following:  
@@ -55,6 +51,17 @@ function getWinners(array, getFinalsCB) {
     return getFinalsCB(array).map(item => item['Home Team Goals'] > item['Away Team Goals'] ? item['Home Team Name'] : item['Away Team Name']);
 }
 console.log(getWinners(fifaData, getFinals));
+//equivalent, ? = 'then,' : = 'else,'
+// function getWinners(array, getFinalsCB) {
+//     return getFinalsCB(array).map(function(item) {
+//         if(item['Home Team Goals'] > item['Away Team Goals']) {
+//             return item['Home Team Name'];
+//         } else {
+//             return item['Away Team Name'];
+//         }
+//     })
+// }
+// console.log(getWinners(fifaData, getFinals));
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 5: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
@@ -104,9 +111,10 @@ Create a function called `getCountryWins` that takes the parameters `data` and `
 Hint: Investigate your data to find "team initials"!
 Hint: use `.reduce` */
 
-function getCountryWins(data, teamInitials) {
-    
-    /* code here */
+//we want to reduce our data to only the team initials, then whatever team initial we put into the parameter, it should return with how many wins its had.
+
+function getCountryWins(data, initials) {
+    // const teamInitials = 
 
 }
 
